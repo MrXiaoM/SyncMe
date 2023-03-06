265e71c59360b0456ed30a018639d494ab0a79cd
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-03-06 13:19:04 +0800

    init table

--- src/main/kotlin/database/DatabaseManager.kt
+++ src/main/kotlin/database/DatabaseManager.kt
@@ -8,7 +8,34 @@ object DatabaseManager {
     internal var server = "default"
     internal lateinit var connection: Connection
 
+    enum class TablePlayerData(
+        val type: String = "longtext"
+    ) {
+        ADVANCEMENTS,
+        INVENTORY,
+        ENDERCHEST,
+        POTIONS,
+        LEVEL("text(64)"),
+        STATISTIC;
+
+        companion object {
+            val table
+                get() = "${prefix}player_dat"
+
+            fun check(conn: Connection) {
+                val s = conn.createStatement()
+                s.executeUpdate("""
+                        CREATE TABLE IF NOT EXISTS $table 
+                        (`name` VARCHAR(64) NOT NULL, ${values().joinToString { "`${it.name.lowercase()}` ${it.type}" }}, PRIMARY KEY (`name`));
+                    """.trimIndent()
+                )
+                s.close()
+            }
+        }
+    }
+
     fun init(conn: Connection) {
         connection = conn
+        TablePlayerData.check(conn)
     }
 }
