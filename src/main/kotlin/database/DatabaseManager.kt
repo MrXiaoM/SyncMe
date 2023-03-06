eaba08d9b4bf0c13c5e4c20d2054d60e45bcf5e5
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-03-06 13:20:43 +0800

    support push and pull data

--- src/main/kotlin/database/DatabaseManager.kt
+++ src/main/kotlin/database/DatabaseManager.kt
@@ -1,6 +1,10 @@
 package top.mrxiaom.syncme.database
 
+import kotlinx.coroutines.async
+import org.bukkit.OfflinePlayer
 import top.mrxiaom.syncme.SyncMe
+import top.mrxiaom.syncme.database.DatabaseManager.Type.*
+import top.mrxiaom.syncme.nms.NMS
 import java.sql.Connection
 
 object DatabaseManager {
@@ -60,4 +64,69 @@ object DatabaseManager {
             }
         }
     }
+
+    suspend fun push(p: OfflinePlayer, vararg types: Type): Boolean = SyncMe.async {
+        val player = NMS.current.loadPlayer(p) ?: return@async false
+        val values = NMS.current.run {
+            types.map {
+                when (it) {
+                    ADVANCEMENTS -> getAdvancements(player)
+                    INVENTORY -> getInventory(player)
+                    ENDERCHEST -> getEnderChest(player)
+                    POTIONS -> getPotions(player)
+                    LEVEL -> player.exp.toString()
+                    STATISTIC -> ""
+                }
+            }
+        }
+        val insertColumns = types.joinToString { "`${it.name.lowercase()}`" }
+        val insertValues = types.joinToString { "?" }
+        val updateValues = types.joinToString { "`${it.name.lowercase()}` = ?" }
+        val sql = """
+            INSERT INTO ${TablePlayerData.table} (`name`, $insertColumns) 
+            VALUES (?, $insertValues) 
+            ON DUPLICATE KEY UPDATE $updateValues
+            """.trimIndent()
+        val ps = connection.prepareStatement(sql)
+        ps.setObject(0, player.name.lowercase())
+        for (i in values.indices) {
+            ps.setObject(i + 1, values[i])
+            ps.setObject(i + 1 + values.size, values[i])
+        }
+        ps.executeUpdate()
+        ps.close()
+        return@async true
+    }.await()
+
+    suspend fun pull(p: OfflinePlayer, vararg types: Type): Boolean = SyncMe.async {
+        val player = NMS.current.loadPlayer(p) ?: return@async false
+        val selectColumns = types.joinToString { "`${it.name.lowercase()}`" }
+        val sql = """
+            SELECT $selectColumns FROM ${TablePlayerData.table} 
+            WHERE `name` = ?
+        """.trimIndent()
+        val ps = connection.prepareStatement(sql)
+        ps.setObject(0, p)
+        val result = ps.executeQuery()
+        if (!result.next()) {
+            result.close()
+            ps.close()
+            return@async false
+        }
+        NMS.current.apply {
+            for (type in types) {
+                val s = result.getObject(type.name.lowercase()).toString()
+                when (type) {
+                    ADVANCEMENTS -> setAdvancements(player, s)
+                    INVENTORY -> setInventory(player, s)
+                    ENDERCHEST -> setEnderChest(player, s)
+                    POTIONS -> setPotions(player, s)
+                    LEVEL -> player.exp = s.toFloat()
+                    STATISTIC -> Unit
+                }
+            }
+            savePlayer(player)
+        }
+        return@async true
+    }.await()
 }
